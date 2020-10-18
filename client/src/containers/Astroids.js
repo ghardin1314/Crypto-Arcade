634b7fb362b1f1dcef989f123187f7b9dbd63df6
ghardin1314 <g.hardin1314@gmail.com>
2020-10-18 15:14:22 -0700

    Final update to client before posting

--- client/src/containers/Astroids.js
+++ client/src/containers/Astroids.js
@@ -18,7 +18,7 @@ import Leaderboard from "../components/Reacteroids/Leaderboard";
 import SignUpModal from "../components/Authentication/SignUpModal";
 
 const useStyles = makeStyles((theme) => ({
-  base:{
+  base: {
     display: "flex",
     backgroundImage: `url(https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg?cs=srgb&dl=pexels-adrien-olichon-2387793.jpg&fm=jpg)`,
     maxWidth: "100vw",
@@ -31,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexFlow: "column",
-    marginTop: 64
+    marginTop: 64,
   },
   container: {
     display: "flex",
@@ -132,139 +132,158 @@ export default function Astroids() {
 
   return (
     <Grid container className={classes.base} justify="center" align="center">
-    <div ref={refEl} className={classes.root}>
-      {state.gameType === "paid" ? <ReacteroidsClient /> : <Reacteroids />}
-      <div style={{ padding: 20 }}>
-        <Grid
-          className={classes.directions}
-          container
-          justify="center"
-          spacing={2}
-        >
-          <Grid item>
-            <Paper className={classes.paper} variant="outlined" elevation={0}>
-              <Typography className={classes.typo} variant="subtitle1">
-                Use [A][W][D] or [←][↑][→] to MOVE
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item>
-            <Paper className={classes.paper} variant="outlined" elevation={0}>
-              <Typography className={classes.typo} variant="subtitle1">
-                Use [SPACE] to SHOOT
-              </Typography>
-            </Paper>
-          </Grid>
-        </Grid>
-      </div>
-      <Popper
-        open={!state.inGame}
-        disablePortal={true}
-        anchorEl={anchorEl}
-        placement="bottom"
-        className={classes.popper}
-        popperOptions={{
-          modifiers: {
-            offset: {
-              offset: "0,-80%",
-            },
-          },
-        }}
-      >
-        <Paper  className={classes.papper}>
+      <div ref={refEl} className={classes.root}>
+        {state.gameType === "paid" ? <ReacteroidsClient /> : <Reacteroids />}
+        <div style={{ padding: 20 }}>
           <Grid
+            className={classes.directions}
             container
             justify="center"
-            alignContent="center"
-            alignItems="center"
             spacing={2}
-            className={classes.container}
           >
-            <Grid item md>
-              <Typography component="h5" variant="h6">
-                Welcome to Crypto Arcade, an arcade that pays you to win! It
-                works like a traditional arcade, but with a twist. You get one
-                game per token and if you make it into the top 50% of scores by
-                the end of the month, you get a payout! After a cut to fund the
-                dev team, the winnings are paid out quadratically, so the higher
-                your placing, the higher your payout! Scores are reset every
-                month.
-                <br />
-                <br />
-                Crypto Arcade is powered by Etherium and requires the browser
-                extension{" "}
-                <a
-                  target="_blank"
-                  href="https://metamask.io/"
-                  rel="noopener noreferrer"
-                >
-                  MetaMask
-                </a> {" "}
-                to play "paid" games. It is currently in beta and free to play
-                on the Etherium Rinkeby Test Network. Go{" "}
-                <a
-                  target="_blank"
-                  href="https://faucet.rinkeby.io/"
-                  rel="noopener noreferrer"
-                >
-                  here
-                </a>{" "}
-                to get free test Eth to play.
-                <br />
-                <br />
-                Dont want to go through the hassle of getting funds? See if you
-                can beat the top scores on the leaderboard in a free game! 
-              </Typography>
+            <Grid item>
+              <Paper className={classes.paper} variant="outlined" elevation={0}>
+                <Typography className={classes.typo} variant="subtitle1">
+                  Use [A][W][D] or [←][↑][→] to MOVE
+                </Typography>
+              </Paper>
             </Grid>
+            <Grid item>
+              <Paper className={classes.paper} variant="outlined" elevation={0}>
+                <Typography className={classes.typo} variant="subtitle1">
+                  Use [SPACE] to SHOOT
+                </Typography>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
+        <Popper
+          open={!state.inGame}
+          disablePortal={true}
+          anchorEl={anchorEl}
+          placement="bottom"
+          className={classes.popper}
+          popperOptions={{
+            modifiers: {
+              offset: {
+                offset: "0,-80%",
+              },
+            },
+          }}
+        >
+          <Paper className={classes.papper}>
             <Grid
               container
               justify="center"
               alignContent="center"
               alignItems="center"
               spacing={2}
-              className={classes.btnContainer}
+              className={classes.container}
             >
-              <Grid item>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  onClick={() => startGame("paid")}
-                  disabled={
-                    !appState.authenticated ||
-                    !appState.web3 ||
-                    !state.AstroidInstance ||
-                    appState.tokens === "0"
-                  }
-                >
-                  Play Paid
-                </Button>
+              <Grid item md>
+                <Typography component="h5" variant="h6">
+                  Welcome to Crypto Arcade, an arcade that pays you to win! It
+                  works like a traditional arcade, but with a twist. You get one
+                  game per token and if you make it into the top 50% of scores
+                  by the end of the month, you get a payout! After a cut to fund
+                  the dev team, the winnings are paid out quadratically, so the
+                  higher your placing, the higher your payout! Scores are reset
+                  every month.
+                  <br />
+                  <br />
+                  Crypto Arcade is powered by Etherium and requires the browser
+                  extension{" "}
+                  <a
+                    target="_blank"
+                    href="https://metamask.io/"
+                    rel="noopener noreferrer"
+                  >
+                    MetaMask
+                  </a>{" "}
+                  to play "paid" games. It is currently in beta and free to play
+                  on the Etherium Rinkeby Test Network. Go{" "}
+                  <a
+                    target="_blank"
+                    href="https://faucet.rinkeby.io/"
+                    rel="noopener noreferrer"
+                  >
+                    here
+                  </a>{" "}
+                  to get free test Eth to play.
+                  <br />
+                  <br />
+                  If you want to learn more about the project, check out my {" "}
+                  <a
+                    target="_blank"
+                    href="https://medium.com/@g.hardin1314/crypto-arcade-my-first-ethereum-dapp-where-high-scores-pay-f78a11d74a8"
+                    rel="noopener noreferrer"
+                  >
+                    Medium article walkthrough
+                  </a>
+                  , or the project{" "}
+                  <a
+                    target="_blank"
+                    href="https://github.com/ghardin1314/Crypto-Arcade"
+                    rel="noopener noreferrer"
+                  >
+                    Github repository
+                  </a>{" "}
+                  . Feel free to leave a star while you're there!
+                  <br />
+                  <br />
+                  Dont want to go through the hassle of getting funds? See if
+                  you can beat the top scores on the leaderboard in a free game!
+                </Typography>
               </Grid>
-              <Grid item>
-                <Button
-                  color="primary"
-                  variant="contained"
-                  onClick={() => startGame("free")}
-                >
-                  Play Free
-                </Button>
-              </Grid>
+              <Grid
+                container
+                justify="center"
+                alignContent="center"
+                alignItems="center"
+                spacing={2}
+                className={classes.btnContainer}
+              >
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => startGame("paid")}
+                    disabled={
+                      !appState.authenticated ||
+                      !appState.web3 ||
+                      !state.AstroidInstance ||
+                      appState.tokens === "0"
+                    }
+                  >
+                    Play Paid
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={() => startGame("free")}
+                  >
+                    Play Free
+                  </Button>
+                </Grid>
 
-              <Grid item>
-                <Button
-                  color="primary"
-                  variant="contained"
-                  onClick={showLeaderboard}
-                >
-                  View Leaderboard
-                </Button>
+                <Grid item>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={showLeaderboard}
+                  >
+                    View Leaderboard
+                  </Button>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </Paper>
-      </Popper>
-      {state.leaderboardOpen ? <Leaderboard /> : null}
-      <SignUpModal />
-    </div>
+          </Paper>
+        </Popper>
+        {state.leaderboardOpen ? <Leaderboard /> : null}
+        <SignUpModal />
+      </div>
     </Grid>
   );
 }
